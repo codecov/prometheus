c43ac42a44f563a49fa8d262666a6a79d59a683a
Josh Pollara <joshpollara@gmail.com>
2018-06-26 17:01:53 +0200

    additional prometheus entrypoint args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-prometheus --web.listen-address ":9090" --config.file="/etc/prometheus/prometheus.yml" "$@"
+/bin/prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus --web.console.libraries=/usr/share/prometheus/console_libraries --web.console.templates=/usr/share/prometheus/consoles --storage.tsdb.no-lockfile
